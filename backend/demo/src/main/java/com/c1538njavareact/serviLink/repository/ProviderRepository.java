657c5853b7411a6e5ce8c9723d17670fbd8b234c
Juan Jose Ceballos Navia <juanfix@gmail.com>
2023-12-03 22:24:43 -0500

    Spring Security was implemented

--- backend/demo/src/main/java/com/c1538njavareact/serviLink/repository/ProviderRepository.java
+++ backend/demo/src/main/java/com/c1538njavareact/serviLink/repository/ProviderRepository.java
@@ -1,7 +1,11 @@
 package com.c1538njavareact.serviLink.repository;
 
+import com.c1538njavareact.serviLink.model.entity.Provider;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ProviderRepository {
+public interface ProviderRepository extends JpaRepository<Provider, Long> {
+    UserDetails findByEmail(String email);
 }
